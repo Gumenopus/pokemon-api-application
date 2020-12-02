db822f425b178616c9f7e9f0961fc9c8dfe746b9
Gustabox <gustavo.dickmann123@gmail.com>
2020-12-02 19:21:46 -0300

    - application default url changed. getting all cards with image working

--- src/pages/home/components/assets/pokemonAsset/PokemonIcon.styles.ts
+++ src/pages/home/components/assets/pokemonAsset/PokemonIcon.styles.ts
@@ -10,7 +10,7 @@ const useStyles = makeStyles(() =>
       alignItems: 'center',
 
       width: '100px',
-      height: '100px',
+      height: '150px',
     },
   }),
 );
--- src/pages/home/components/cardPokemon/CardPokemon.tsx
+++ src/pages/home/components/cardPokemon/CardPokemon.tsx
@@ -11,15 +11,15 @@ import useStyles from './CardPokemon.styles';
 
 import PokemonIcon from '../assets/pokemonAsset/PokemonIcon';
 import { Link } from 'react-router-dom';
-import { createNamedExports, JSDocCallbackTag } from 'typescript';
+import { createNamedExports, JSDocCallbackTag, ListFormat } from 'typescript';
 import Box from '@material-ui/core/Box';
 import Pokemon from 'model/Pokemon';
 
 interface Props {
-  results: [
+  cards: [
     {
       name: string;
-      url: string;
+      imageUrl: string;
     },
   ];
 }
@@ -28,41 +28,22 @@ const CardPokemon = () => {
   const { container, textStyle } = useStyles();
   const [pokemons, setPokemons] = useState<Props>();
 
-  async function addAllPokemonsInList(
-    quantityOfPokemons: number,
-  ): Promise<void> {
-    const response = await api.get<Props>(
-      `/pokemon/?limit=${quantityOfPokemons}&offset=0`,
-    );
+  async function addAllPokemonsInList(): Promise<void> {
+    const response = await api.get<Props>('/cards');
     setPokemons(response.data);
   }
 
   useEffect(() => {
-    addAllPokemonsInList(500);
+    addAllPokemonsInList();
   }, []);
-
-  function sendPokemonIdToHistory(id: number) {
-    // When user clicks on it, will send to another screen the id value. Path will need to change with id value.
-    <Link
-      to={{
-        pathname: `/details/${id}`,
-        state: `${id}`,
-      }}
-    ></Link>;
-  }
-
-  const list = [];
-
-  list.push('aaa');
-  list.push('bbb');
 
   return (
     <Fragment>
-      {pokemons?.results.map(pokemon => (
+      {pokemons?.cards.map(card => (
         <Grid className={container}>
           <Paper elevation={1} className={textStyle}>
-            <Typography>Name: {pokemon.name}</Typography>
-            <PokemonIcon src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/445.png"></PokemonIcon>
+            <Typography>Name: {card.name}</Typography>
+            <PokemonIcon src={card.imageUrl}></PokemonIcon>
           </Paper>
         </Grid>
       ))}
